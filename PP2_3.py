8f8b1320dd75a5afb6ba4e05d725d8c88b2a911c
Nithin-Alagar-ICD2O1-01 <nithin.alagar@student.tdsb.on.ca>
2024-10-16 14:02:03 +0000

    nithin

--- PP2_3.py
+++ PP2_3.py
@@ -2,6 +2,16 @@
 
 def q1(): 
   #Write Assignment code here
+  word = input("input word: ")
+  if word[-1:] == "ife":
+    print("-ives")
+  elif word[-2:] == "ey":
+    print("-eys")
+  elif word[-3:] == "y":
+    print("-ies")
+  else:
+    print("s")
+  
 
 
 def q2(): 
@@ -12,5 +22,5 @@ def q2():
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
+#q1()
+#q2()
